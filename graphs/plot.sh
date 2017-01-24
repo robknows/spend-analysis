85a13525df25311c40822886fb3bbc693d057ec2
Rob <robmoore121@gmail.com>
2017-01-24 12:35:56 +0000

    Rewording a comment

--- graphs/plot.sh
+++ graphs/plot.sh
@@ -2,10 +2,10 @@
 # Update data
 ./saveplotdata.q
 
-# Single plot all of it
+# Single plot all the data
 graphdata=`ls graphdata/*.txt`
 ./makesingleplots.ijs $graphdata
 
-# Double plot selected ones
+# Double plot only selected ones
 ./makedoubleplots.ijs graphdata/travel_spend_per_day.txt graphdata/gym_spend_per_day.txt
 
